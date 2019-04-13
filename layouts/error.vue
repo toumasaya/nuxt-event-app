514126b94f65e6ab38e74eb265b9a781bbcb60b9
Salt <luffy.w.pirate@gmail.com>
2019-04-13 10:35:49 +0800

    Add error page

---
+++ layouts/error.vue
@@ -0,0 +1,38 @@
+<template lang="pug">
+  section.section
+    .container
+      b-icon(icon="alert-circle" size="is-large")
+      h1.title {{ message }}
+      p(v-if="statusCode === 404")
+        nuxt-link(to="/") Return to homepage
+</template>
+
+<script>
+export default {
+  props: {
+    error: {
+      type: Object,
+      default: null
+    }
+  },
+  head() {
+    return {
+      title: this.message
+    }
+  },
+  computed: {
+    statusCode() {
+      return (this.error && this.error.statusCode) || 500
+    },
+    message() {
+      return this.error.message
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+.container {
+  text-align: center;
+}
+</style>
