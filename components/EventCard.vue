cac8405f12e42429234654b80fc2b5b429990ac3
Salt <luffy.w.pirate@gmail.com>
2019-04-13 11:13:02 +0800

    Make api call with axios

---
+++ components/EventCard.vue
@@ -0,0 +1,29 @@
+<template lang="pug">
+  .column
+    .card
+      .card-header
+        p.card-header-title {{ event.title }}
+      .card-content
+        p @{{ event.time }} on {{ parsedDate }}
+        p {{ event.attendees.length }} attending
+      footer.card-footer
+        nuxt-link.card-footer-item(:to="'/event/' + event.id") Check Event
+
+</template>
+
+<script>
+export default {
+  name: 'EventCard',
+  props: {
+    event: Object
+  },
+  computed: {
+    parsedDate() {
+      const eventDate = new Date(this.event.date)
+      return eventDate.toDateString()
+    }
+  }
+}
+</script>
+
+<style></style>
--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,48 @@
 <template lang="pug">
-  h1.title Events
+  div
+    h1.title Events
+    .columns
+      EventCard(
+        v-for="(event, index) in events" 
+        :key="index"
+        :event="event"
+        :data-index="index")
 </template>
 
 <script>
+import EventCard from '~/components/EventCard.vue'
+
 export default {
+  components: {
+    EventCard
+  },
   head() {
     return {
       title: 'Event Lists'
     }
+  },
+  asyncData({ $axios, error }) {
+    return $axios
+      .get('http://localhost:4000/events')
+      .then(response => {
+        return {
+          events: response.data
+        }
+      })
+      .catch(e => {
+        error({
+          statusCode: 503,
+          message: 'Unable to fetch events at this time. Please try again.'
+        })
+      })
   }
+  // asyncData(context) {
+  //   return context.$axios.get('http://localhost:4000/events').then(response => {
+  //     return {
+  //       events: response.data
+  //     }
+  //   })
+  // }
 }
 </script>
 
